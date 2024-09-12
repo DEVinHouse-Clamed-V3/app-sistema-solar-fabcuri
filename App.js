fc62f25bc8a2dc2f28fc6bc89e219c08779a531f
Fabcuri <fabcuri70@gmail.com>
2024-09-12 11:32:34 -0300

    criando pagina app.js

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar} from 'expo-status-bar';
+import { StyleSheet, Text, View,SafeAreaView, Image } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <View style={styles.row}>
+        <StatusBar backgroundColor="#0077B5" />
+        <Text style={styles.myName}>Vamos Explorar</Text>
+        <Image
+          source={require('./assets/Astronauta.jpg')}
+          style={styles.myImage}
+        />
+        </View>
+
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    padding: 20,
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#8dbdeb',
+    marginTop:25
+   
+
+  },
+  myImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    alignSelf: 'center',
+    marginVertical: 10
+  },
+  row:
+  {
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
+    gap: 10
+
+
+  },
+  myName: {
+    color: '#FFF',
+    fontSize: 35,
+    textAlign: 'center',
+
+    
   },
 });
